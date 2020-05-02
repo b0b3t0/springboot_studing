8a3aaef7827ae4d4ab9a685783191c686a9b5be1
Bojidar Nenov <cruelbobo@abv.bg>
2020-05-02 19:41:06 +0300

    Created User repository

---
+++ src/main/java/com/exercises/demo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.exercises.demo.repository;
+
+import com.exercises.demo.domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    User findByUsername(String username);
+}
