e66151e9efd42d256eb3029edc5cbde064984e9a
Bojidar Nenov <cruelbobo@abv.bg>
2020-05-02 19:19:39 +0300

    Created UserController for login, center the login form + added merge top on the footer

---
+++ src/main/java/com/exercises/demo/web/controllers/UserController.java
@@ -0,0 +1,15 @@
+package com.exercises.demo.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController extends BaseController{
+    @GetMapping("/login")
+    public ModelAndView login() {
+        return this.view("login");
+    }
+
+
+}
