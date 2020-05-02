2083577705e5073cb494bae4a927876560191389
Bojidar Nenov <cruelbobo@abv.bg>
2020-05-02 19:49:00 +0300

    Created ApplicationConfiguration file and added bean of Modelmapper

---
+++ src/main/java/com/exercises/demo/config/ApplicationConfig.java
@@ -0,0 +1,4 @@
+package com.exercises.demo.config;
+
+public class ApplicationConfig {
+}
---
+++ src/main/java/com/exercises/demo/domain/models/bindings/RegisterUserBindingModel.java
@@ -0,0 +1,50 @@
+package com.exercises.demo.domain.models.bindings;
+
+import javax.validation.constraints.NotNull;
+
+public class RegisterUserBindingModel {
+    private String username;
+    private String email;
+    private String password;
+    private String confirmPassword;
+
+    public RegisterUserBindingModel() {
+
+    }
+
+    @NotNull
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @NotNull
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @NotNull
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @NotNull
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
